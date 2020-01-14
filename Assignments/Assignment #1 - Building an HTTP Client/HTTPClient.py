0b65031fca7eaf78d88e5545ca5e120058cbbf23
ew0k <jakebrown188@gmail.com>
2020-01-14 08:37:15 -0500

    Gather input from command line

--- Assignments/Assignment #1 - Building an HTTP Client/HTTPClient.py
+++ Assignments/Assignment #1 - Building an HTTP Client/HTTPClient.py
@@ -1,9 +1,35 @@
 import sys
+import logging
 
+
+
+def print_usage():
+    print("Usage: HTTPClient.py URL")
+
+
+def validate_url(url):
+    return True
 
 def gather_input():
-    
+    if len(sys.argv) != 2:
+        print("Error: Did not supply proper arguments")
+        print_usage()
+        logging.error("Did not supply proper arguments. Arguments supplied: " + str(sys.argv))
+    url = sys.argv[1]
+    if not validate_url(url):
+        print("Error: URL provided is not a valid url")
+        print("Valid URL: <scheme>://<host>:<port>/[path]")
+        print("Scheme must be http or https")
+        logging.error("URL provided not valid: " + url)
+    return url
+
+def configure_logger():
+    logging.basicConfig(level=logging.DEBUG, filemode='a', format='%(asctime)s - [%(levelname)s] - %(message)s', filename='HTTPClient.log')
 
 
 def main():
-    gather_input()
+    configure_logger()
+    url = gather_input()
+
+
+main()
