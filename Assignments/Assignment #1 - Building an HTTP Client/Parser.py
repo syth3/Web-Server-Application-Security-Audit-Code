1e9bb6465a11de0bf70e03e18e2c7f9094871ae3
ew0k <jakebrown188@gmail.com>
2020-01-14 14:24:14 -0500

    Initial Commit

---
+++ Assignments/Assignment #1 - Building an HTTP Client/Parser.py
@@ -0,0 +1,21 @@
+'''
+file: ConnectionUtils.py
+language: python3
+author: Jacob Brown jmb7438@rit.edu
+description: Parses a web page using only string libraries. Used by HTTPClient.py
+'''
+
+def parse_page_for_external_references(web_page):
+    """Parse a web page and extract external links
+    
+    Parameters
+    ----------
+    web_page : string
+        web page to be parsed
+    
+    Returns
+    -------
+    list
+        list of external references
+    """
+    return []
