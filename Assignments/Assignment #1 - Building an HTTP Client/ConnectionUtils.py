91a95e5ceb216f5d8ac786d4167eeec0094a2629
ew0k <jakebrown188@gmail.com>
2020-01-14 11:36:47 -0500

    Update docstring

---
+++ Assignments/Assignment #1 - Building an HTTP Client/ConnectionUtils.py
@@ -0,0 +1,11 @@
+'''
+file: ConnectionUtils.py
+language: python3
+author: Jacob Brown jmb7438@rit.edu
+description: Utilities for connecting to a URL over a python socket. Used by HTTPClient.py
+'''
+
+import socket
+
+def test():
+    print("Test")
